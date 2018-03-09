4d4fa16626f31ccc40407bbd912e79f2da279303
kari-antti seppälä <kariantti.s@gmail.com>
2018-03-09 13:30:33 +0200

    timetables
    json muotoilu

--- src/Classes/TimeTables.js
+++ src/Classes/TimeTables.js
@@ -1,12 +1,16 @@
 import React from 'react';
-
 export class TimeTables extends React.Component {
   constructor(props) {
   super(props);
   this.state = {
     Stop_Code: '',
-    Line_Code: '',
-    Stop_Times: '',
+    day: '',
+    hour: '',
+    minute: '',
+    line: '',
+    Stop_Time: '',
+
+
   };
 
   this.handleChange = this.handleChange.bind(this);
@@ -15,6 +19,7 @@ export class TimeTables extends React.Component {
 
 handleChange(event) {
   var change = {}
+  console.log(event.target.value);
     change[event.target.name] = event.target.value
     this.setState(change)
 }
@@ -22,17 +27,30 @@ handleChange(event) {
 handleSubmit(event) {
 //  console.log(this);
   event.preventDefault();
+
+  var timeStr = this.state.Stop_Time;
+  var splitTime = timeStr.split(':');
+  this.state.hour = splitTime[0];
+  this.state.minute = splitTime[1];
+
+
   fetch('/timetables/new', {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
       'Content-Type':'application/json'},
 
+
+
     body: JSON.stringify({
       Stop_Code: this.state.Stop_Code,
-      Line_Number: this.state.Line_Code,
-      Stop_Times:this.state.Stop_Times
 
+      stopTimesObj: { dayType : this.state.day,
+                    hours : {
+                      hour: this.state.hour ,
+                      allData: { data: {min: this.state.minute, Line_Code: this.state.line} }
+                    }
+                  }
   }),
   })
   .then(function(res){ return res.json(); })
@@ -42,7 +60,7 @@ render() {
   return (
     <div>
     <p>Timetables Form</p>
-          <form onSubmit={this.handleSubmit}>
+          <form onSubmit={this.handleSubmit} id="timetables_form">
             <label htmlFor="Stop_Code">Stop Code
             <input type="text" name="Stop_Code" id="Stop_Code" onChange={this.handleChange.bind(this)} value={this.state.name}/>
             </label><br/>
@@ -51,12 +69,23 @@ render() {
             <input type="text" name="Line_Code" id="Line_Code" onChange={this.handleChange.bind(this)} value={this.state.name}/>
             </label><br/>
 
-            <label htmlFor="Stop_Times">Stop Times
-            <input type="text" name="Stop_Times" id="Stop_Times" onChange={this.handleChange.bind(this)} value={this.state.name}/>
+            <label htmlFor="Stop_Time">Stop Times
+            <input type="time" name="Stop_Time" id="Stop_Time" onChange={this.handleChange.bind(this)} value={this.state.name}/>
             </label><br/>
+
+            <div>
+            Day
+            <select name="day" form="timetables_form">
+            <option value="weekday">Weekday</option>
+            <option value="saturday">Saturday</option>
+            <option value="sunday">Sunday</option>
+            </select>
+            </div>
 
             <input type="submit" value="Submit" />
           </form>
+
+
     </div>
   );
 }
