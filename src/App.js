1481d1f969b2fbf79dc40aeb5ce2f7b9644d6acf
kari-antti seppälä <kariantti.s@gmail.com>
2018-03-09 15:29:36 +0200

    Maps toimii
    maps toimii

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import Post from './PostNew.js';
+import { GoogleApiWrapper } from 'google-maps-react'
+import MapContainer from './MapContainer'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <p>hello world</p>
-      <br />
-        <Post/>
+      <div>
+
+
+        <MapContainer google={this.props.google} />
+
       </div>
     );
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBWhKVQtFGxlTcrO3idYsuix-RljyDyenk',
+})(App)
