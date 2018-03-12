510194aba51c7e5ba995542fd589903a8d9a9eb5
markuspokkinen <markuspokkinen@gmail.com>
2018-03-12 10:29:32 +0200

    iso haku
    ei viel toimi, enkä tiedä miksi

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -3,13 +3,31 @@ import React, { Component } from 'react';
 export default class searchBar extends Component {
   constructor(props) {
   super(props);
+  this.state = {
+    list: ""
+  };
+  this.handleChange = this.handleChange.bind(this);
+}
+handleChange(event){
+  var arr = [];
+  fetch("/routes/"+event.target.value).then(res => res.json()).then((result) => {
+    const data = result.map((line) =><option value={line.Line_Code}/>);
+    data.map((tieto) => arr.push(tieto));
+  });
 
+  fetch("/stops/"+event.target.value).then(res => res.json()).then((result) => {
+    const data = result.map((line) =><option value={line.Stop_Code}/>);
+    data.map((tieto) => arr.push(tieto));
+  });
+  console.log(arr);
+  this.setState({list: arr});
 }
 
   render() {
     return (
       <div>
-      <input type="text" placeholder="Search..." style={{width: '100%', height: 30}}/>
+      <input type="text" placeholder="Search..."id="Search" list={"SandR"} onChange={this.handleChange.bind(this)} style={{width: '100%', height: 30}}/>
+      <datalist id ="SandR">{this.state.list}</datalist>
       </div>
     )
   }
