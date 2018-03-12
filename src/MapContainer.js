587b2986deb398a43df3001c6f2866f9ea99f46a
kari-antti seppälä <kariantti.s@gmail.com>
2018-03-12 15:49:39 +0200

    marker näkyvissä
    marker kartalle

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -3,11 +3,34 @@ import ReactDOM from 'react-dom'
 
 export default class MapContainer extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: ''
+    }
+  };
+
+  fetchStops() {
+  fetch("/stops/").then(res =>res.json()).then((result)=>{
+      this.setState({list: result})
+    });
+  }
+
+  componentDidMount() {
+    //loading stops from the database
+    this.fetchStops();
+    console.log(this.state.list);
+  }
+
   componentDidUpdate() {
     this.loadMap(); // call loadMap function to load the google map
   }
 
+
+
+
   loadMap() {
+
     if (this.props && this.props.google) { // checks to make sure that props have been passed
       const {google} = this.props; // sets props equal to google
       const maps = google.maps; // sets maps to google maps props
@@ -18,15 +41,35 @@ export default class MapContainer extends Component {
       const mapConfig = Object.assign({}, {
 
 
-        center: {lat: 60.1651157, lng: 24.9412466}, // sets center of google map to NYC.
+        center: {lat: 60.1651157, lng: 24.9412466}, // sets center of google map to Helsinki.
         zoom: 13, // sets zoom. Lower numbers are zoomed further out.
         mapTypeId: 'roadmap' // optional main map layer. Terrain, satellite, hybrid or roadmap--if unspecified, defaults to roadmap.
       })
 
       this.map = new maps.Map(node, mapConfig); // creates a new Google map on the specified node (ref='map') with the specified configuration set above.
 
+      var markerArr = this.state.list;
+      // creates markers
+      for (var i = 0; i < markerArr.length; i++) {
+        const marker = new google.maps.Marker({
+          position: {
+            lat: parseFloat(markerArr[i].Position.lat),
+            lng: parseFloat(markerArr[i].Position.lng) },
+          map: this.map,
+          title: markerArr[i].Name
+        });
+        console.log(marker);
+
+      }
+
+
+
     }
+
   }
+
+
+
 
   render() {
     const style = { // MUST specify dimensions of the Google map or it will not work. Also works best when style is specified inside the render function and created as an object
